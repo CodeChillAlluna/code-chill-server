c2d62345a63e0e084f0e35f89a4a30d753ae0448
Lulu300 <ludwigsimon77@gmail.com>
2018-11-22 18:39:08 +0100

    Fix docker install and update to java 11

--- install/install.sh
+++ install/install.sh
@@ -24,41 +24,57 @@ docker_install () {
   sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
   sudo apt-get update
   sudo apt-get install docker-ce -y
-  sudo su -
-  echo -e "[Unit]
-  Description=Docker Application Container Engine
-  Documentation=https://docs.docker.com
-  After=network-online.target docker.socket firewalld.service
-  Wants=network-online.target
-  Requires=docker.socket\n
-  [Service]
-  Type=notify
-  # the default is not to use systemd for cgroups because the delegate issues still
-  # exists and systemd currently does not support the cgroup feature set required
-  # for containers run by docker
-  ExecStart=/usr/bin/dockerd -H=tcp://0.0.0.0:2375
-  ExecReload=/bin/kill -s HUP \$MAINPID
-  LimitNOFILE=1048576
-  # Having non-zero Limit*s causes performance problems due to accounting overhead
-  # in the kernel. We recommend using cgroups to do container-local accounting.
-  LimitNPROC=infinity
-  LimitCORE=infinity
-  # Uncomment TasksMax if your systemd version supports it.
-  # Only systemd 226 and above support this version.
-  TasksMax=infinity
-  TimeoutStartSec=0
-  # set delegate yes so that systemd does not reset the cgroups of docker containers
-  Delegate=yes
-  # kill only the docker process, not all processes in the cgroup
-  KillMode=process
-  # restart the docker process if it exits prematurely
-  Restart=on-failure
-  StartLimitBurst=3
-  StartLimitInterval=60s\n
-  [Install]
-  WantedBy=multi-user.target" > /lib/systemd/system/docker.service
+  sudo usermod -aG docker ${USER}
+  # sudo su -
+  sudo echo -e "[Unit]
+Description=Docker Application Container Engine
+Documentation=https://docs.docker.com
+BindsTo=containerd.service
+After=network-online.target firewalld.service
+Wants=network-online.target
+
+[Service]
+Type=notify
+# the default is not to use systemd for cgroups because the delegate issues still
+# exists and systemd currently does not support the cgroup feature set required
+# for containers run by docker
+ExecStart=/usr/bin/dockerd -H=tcp://0.0.0.0:2375
+ExecReload=/bin/kill -s HUP $MAINPID
+TimeoutSec=0
+RestartSec=2
+Restart=always
+
+# Note that StartLimit* options were moved from "Service" to "Unit" in systemd 229.
+# Both the old, and new location are accepted by systemd 229 and up, so using the old location
+# to make them work for either version of systemd.
+StartLimitBurst=3
+
+# Note that StartLimitInterval was renamed to StartLimitIntervalSec in systemd 230.
+# Both the old, and new name are accepted by systemd 230 and up, so using the old name to make
+# this option work for either version of systemd.
+StartLimitInterval=60s
+
+# Having non-zero Limit*s causes performance problems due to accounting overhead
+# in the kernel. We recommend using cgroups to do container-local accounting.
+LimitNOFILE=infinity
+LimitNPROC=infinity
+LimitCORE=infinity
+
+# Comment TasksMax if your systemd version does not supports it.
+# Only systemd 226 and above support this option.
+TasksMax=infinity
+
+# set delegate yes so that systemd does not reset the cgroups of docker containers
+Delegate=yes
+
+# kill only the docker process, not all processes in the cgroup
+KillMode=process
+
+[Install]
+WantedBy=multi-user.target
+" > /lib/systemd/system/docker.service
   sudo systemctl daemon-reload
-  sudo service docker restart
+  sudo systemctl restart docker
   export DOCKER_HOST=tcp://localhost:2375
   if ! grep -qF "DOCKER_HOST=tcp://localhost:2375" /etc/environment
   then
@@ -120,7 +136,7 @@ setup () {
   sudo apt-get install -y git
 
   # Install jdk
-  sudo apt-get install -y openjdk-8-jdk openjdk-8-jre
+  sudo apt-get install -y openjdk-11-jdk
 
   # Install build automation
   sudo apt-get install -y maven
